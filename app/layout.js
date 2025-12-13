d3245fc0adc5c7444f52d8b70f418dad118bb036
praveenraj <105029450+praveen-raj-r@users.noreply.github.com>
2025-12-13 23:12:57 +0530

    added favicon, css files , root layout and landing page

---
+++ app/layout.js
@@ -0,0 +1,45 @@
+import { ThemeProvider } from "@/components/theme-provider";
+import "./globals.css";
+import Header from "@/components/header";
+import Footer from "@/components/footer";
+import { ConvexClientProvider } from "../components/convex-client-provider";
+import { ClerkProvider } from "@clerk/nextjs";
+
+export const metadata = {
+  title: "Spott",
+  description: "Discover and create amazing events",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <ClerkProvider>
+      <html lang="en" suppressHydrationWarning>
+        <body className="bg-linear-to-br from-gray-950 via-zinc-900 to-stone-900 text-white">
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <ConvexClientProvider>
+              <Header />
+
+              <main className="relative min-h-screen container mx-auto pt-40 md:pt-32">
+                {/* Background glow effects (behind everything) */}
+                <div className="pointer-events-none absolute inset-0 -z-10 overflow-hidden">
+                  <div className="absolute top-0 left-1/4 w-96 h-96 bg-pink-600/20 rounded-full blur-3xl" />
+                  <div className="absolute bottom-0 right-1/4 w-96 h-96 bg-orange-600/20 rounded-full blur-3xl" />
+                </div>
+
+                {/* Page content (above glow) */}
+                <div className="relative z-10">{children}</div>
+              </main>
+
+              <Footer />
+            </ConvexClientProvider>
+          </ThemeProvider>
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
