dab8e087ff28dbf7832f515948297aed89dc7a27
praveenraj <105029450+praveen-raj-r@users.noreply.github.com>
2026-01-03 16:57:07 +0530

    refactor

--- hooks/use-store-user.jsx
+++ hooks/use-store-user.jsx
@@ -7,17 +7,17 @@ import { api } from "../convex/_generated/api";
 export function useStoreUser() {
   const { isLoading, isAuthenticated } = useConvexAuth();
   const { user } = useUser();
-  // When this state is set we know the server
-  // has stored the user.
+
+  // When this state is set we know the server has stored the user.
   const [userId, setUserId] = useState(null);
   const storeUser = useMutation(api.users.store);
+
   // Call the `storeUser` mutation function to store
   // the current user in the `users` table and return the `Id` value.
+
   useEffect(() => {
-    // If the user is not logged in don't do anything
-    if (!isAuthenticated) {
-      return;
-    }
+    if (!isAuthenticated) return; // If the user is not logged in don't do anything
+
     // Store the user in the database.
     // Recall that `storeUser` gets the user information via the `auth`
     // object on the server. You don't need to pass anything manually here.
@@ -25,11 +25,13 @@ export function useStoreUser() {
       const id = await storeUser();
       setUserId(id);
     }
+
     createUser();
+
     return () => setUserId(null);
-    // Make sure the effect reruns if the user logs in with
-    // a different identity
+    // Make sure the effect reruns if the user logs in with a different identity
   }, [isAuthenticated, storeUser, user?.id]);
+
   // Combine the local state with the state from context
   return {
     isLoading: isLoading || (isAuthenticated && userId === null),
